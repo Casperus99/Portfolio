449fe6944ec9c0c04c6689bdbfc7d5c1731a8399
Casperus <Casperus99@wp.pl>
2022-10-31 18:30:25 +0100

    Data preprocessing

--- AI algorithms/EVO-Tree Algorithm/Load_data.py
+++ AI algorithms/EVO-Tree Algorithm/Load_data.py
@@ -1,114 +1,114 @@
 #-*- conding: utf-8 -*-
 import csv
-import os
+from msilib.schema import Feature
 import random
 import math
 
-class Object:
+import os
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn import metrics
 
-    def __init__(self, features, target):
-        self.features = features
-        self.target = target
+from six import StringIO  
+from IPython.display import Image  
+import pydotplus
+import matplotlib.pyplot as plt
 
+class DataHandler:
 
-class Dataset:
-
-    def __init__(self, file):
-        self.data = []
-        self.train = []
-        self.test = []
-        self.ft_quantity = 0
-        self.available_targets = []
-        self.__load_data(file)
-
-    '''
-    Wczytuje plik csv i uzupełnia zbiór danych obiektami
-    '''
-    def __load_data(self, file):
+    def loadDataFrom(self, fileName):
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        with open(os.path.join(__location__, file), newline='') as f:
-            reader = csv.reader(f)
-            self.data = list(reader)
-        # Dla każdego rekordu
-        for i, record in enumerate(self.data):
-            # Zamień wszystkie jego wartości na floaty
-            for j, value in enumerate(record):
-                self.data[i][j] = float(value)
-            # Zamień go na obiekt klasy Object
-            self.data[i] = Object(self.data[i][:-1], self.data[i][-1])
-        # Zlicz liczbę atrybutów
-        self.ft_quantity = len(self.data[0].features)
-        # Znajdź wszystkie dostępne klasy
-        for object in self.data:
-            if object.target not in self.available_targets:
-                self.available_targets.append(object.target)
-        self.available_targets.sort()
+        self.dataset = pd.read_csv(os.path.join(__location__, fileName))
+        self.target = self.dataset.columns[-1]
 
-    def split_data(self, ratio):
-        random.shuffle(self.data)
-        local_train = []
-        local_test = []
-        threshold = math.ceil(ratio * len(self.data))
-        for i, object in enumerate(self.data):
-            if i <= threshold:
-                local_train.append(object)
-            else:
-                local_test.append(object)
-        self.train = local_train
-        self.test = local_test
+    def getEncodedData(self, ordinalFeaturesMapperDict):
+        oe = self.getOrdinalEncoder(ordinalFeaturesMapperDict)
+        ohe = self.getOneHotEncoder()
+        ct = self.getColumnTransformer(ordinalFeaturesMapperDict, oe, ohe)
+        return ct.fit_transform(self.dataset)
 
-    def find_min_max(self):
-        ft_min_max = []
-        for i in range(self.ft_quantity):
-            ft_min_max.append([99999, -99999])
+    def getCategoricalFeatures(self):
+        featureColumns = self.dataset.drop(self.target, axis=1)
+        categoricalFeatures = featureColumns.select_dtypes(include=['object', 'bool']).columns
+        return categoricalFeatures
 
-        for record in self.train:
-            for index, ft in enumerate(record.features):
-                if ft < ft_min_max[index][0]:
-                    ft_min_max[index][0] = ft
-                elif ft > ft_min_max[index][1]:
-                    ft_min_max[index][1] = ft
+    def getOrdinalFeatures(self, ordinalMappersDict):
+        ordinalFeatures = []
+        for features in ordinalMappersDict.keys():
+            ordinalFeatures.extend(features)
+        return ordinalFeatures
 
-        return ft_min_max
+    def getNominalFeatures(self, ordinalFeatures):
+        categoricalFeatures = self.getCategoricalFeatures()
+        nominalFeatures = [feature for feature in categoricalFeatures if feature not in ordinalFeatures]
+        return nominalFeatures
 
-    '''
-    Normalizuje liniowo wartości atrybutów w przedziale od 0 do 1
-    '''
-    # def normalize_features(self):
-    #     # Do przetrzymywania wartości maksymalnych i minimalnych kolejnych atrybutów
-    #     buffer = []
-    #     # Wypełnij bufor, by go potem móc łatwo aktualizować
-    #     for i in range(self.ft_quantity):
-    #         buffer.append([0, 0])
-    #     # Dla każdego objektu
-    #     for i, object in enumerate(self.data):
-    #         # Dla pierwszego przepisz jego wartości jako min i max jednocześnie
-    #         if i == 0:
-    #             for j, ft_value in enumerate(object.features):
-    #                 buffer[j] = [ft_value, ft_value]
-    #         # Dla kolejnych sprawdź czy któryś z jego atrybutów nie jest nowym min lub max
-    #         else:
-    #             for j, ft_value in enumerate(object.features):
-    #                 if ft_value < buffer[j][0]:
-    #                     buffer[j][0] = ft_value
-    #                 elif ft_value > buffer[j][1]:
-    #                     buffer[j][1] = ft_value
-    #     # Zaktualizuj wszystkie wartości atrybutów we wszystkich obiektach
-    #     for i, object in enumerate(self.data):
-    #         for j, ft_value in enumerate(object.features):
-    #             self.data[i].features[j] =  (self.data[i].features[j] - buffer[j][0]) / (buffer[j][1] - buffer[j][0])
+    def getOrdinalEncoder(self, mapperDict):
+        categories = []
+        for features, mapper in mapperDict.items():
+            for _ in range(len(features)):
+                categories.append(mapper)
+        return OrdinalEncoder(categories=categories)
 
-    '''
-    Zamienia klasy objektów na spodziewane wektory dla sieci neuronowej
-    '''
-    # def neural_network_target(self):
-    #     # Stwórz domyślny spodziewany wektor o odpowiedniej długości
-    #     deafult = []
-    #     for i in range(len(self.available_targets)):
-    #         deafult.append(0)
-    #     # W zależności od klasy obiektu zastąp odpowiedni element wektora jedynką i aktualizuj obiekt
-    #     for i, object in enumerate(self.data):
-    #         buffer = deafult.copy()
-    #         index = self.available_targets.index(object.target)
-    #         buffer[index] = 1
-    #         self.data[i].target = buffer
+    def getOneHotEncoder(self):
+        return OneHotEncoder()
+
+    def getColumnTransformer(self, ordinalMapperDict, ordinalEncoder, oneHotEncoder):
+        ordinalFeatures = self.getOrdinalFeatures(ordinalMapperDict)
+        nominalFeatures = self.getNominalFeatures(ordinalFeatures)    
+        columnTransformer = make_column_transformer(
+            (ordinalEncoder, ordinalFeatures),
+            (oneHotEncoder, nominalFeatures),
+            remainder='passthrough'
+        )
+        return columnTransformer
+
+
+datasetFileName = 'ObesityData.csv'
+ordinalFeaturesMappers = {
+    ("CAEC", "CALC"): ["no", "Sometimes", "Frequently", "Always"]
+}
+
+dataHandler = DataHandler()
+dataHandler.loadDataFrom(datasetFileName)
+readyData = dataHandler.getEncodedData(ordinalFeaturesMappers)
+print(readyData)
+
+# dataHandler.dataset.hist(bins=25,figsize=(10,10))
+# plt.show()
+
+# print(dataHandler.dataset.isnull().values.any())
+
+# dataHandler.dataset
+
+
+
+# kfold = KFold(5, shuffle=True)
+# accs = []
+
+# for i in range(5):
+#     for train, test in kfold.split(readyData):
+#         train_x = readyData.loc[train][features]
+#         train_y = readyData.loc[train][classes]
+#         test_x = readyData.loc[test][features]
+#         test_y = readyData.loc[test][classes]
+        
+#         clf = DecisionTreeClassifier()
+#         clf = clf.fit(train_x, train_y)
+#         pred_y = clf.predict(test_x)
+#         accs.append(metrics.accuracy_score(test_y, pred_y))
+
+# print("mean accuracy", np.mean(accs))
+# print("standard deviation", np.std(accs))
+
+# dot_data = StringIO()
+# export_graphviz(clf, out_file=dot_data,  
+#                 filled=True, rounded=True,
+#                 special_characters=True,feature_names = features,class_names=classes)
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+# graph.write_png('tree.png')
+# Image(graph.create_png())
