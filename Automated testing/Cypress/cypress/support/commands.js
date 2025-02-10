e77ff0649a54da9dafb9cc72b622b4c096ee4cb4
Casperus99 <casperus99@wp.pl>
2025-02-10 20:24:16 +0100

    PCAP - finished + directory changes

--- Automated testing/Cypress/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('createLead', (obj) => {
-    cy.task('salesforceCreate', ({objectType: 'Lead', object: obj}))
-})
-Cypress.Commands.add('createAccount', (obj) => {
-    cy.task('salesforceCreate', ({objectType: 'Account', object: obj}))
-})
-Cypress.Commands.add('deleteAccount', (id) => {
-    cy.task('salesforceDelete', ({objectType: 'Account', id: id}))
-})
